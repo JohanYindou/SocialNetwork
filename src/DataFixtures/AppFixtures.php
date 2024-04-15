ed56e5d748c7fc514754ff3745679157a66dea55
Johan Yindou <jyindou@gmail.com>
2024-04-15 09:36:23 +0200

    "Add fixtures for User, Publication, Commentaire, Message, and Groupe entities using Faker"
    This commit adds fixtures for the User, Publication, Commentaire, Message, and Groupe entities using the Faker
    
    Ceci est la premère version des fixtures et n'est donc pas définitive.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,133 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\Commentaire;
+use App\Entity\Groupe;
+use App\Entity\Publication;
+use App\Entity\Message;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        // Mettre des photos de profil
+        $picturePaths = ['/uploads/users/exemple.jpg', '/uploads/users/exemple-2.jpg'];
+
+
+        /** -------------------------------------------------------------
+         *                      ENTITé USER
+         * 
+         *   Ne pas oublier de rajouter les photos de profil au projet 
+         *   et de compéter l'entité User avec les 
+         *   propriétés dont ont a besoin.
+         * 
+         * ---------------------------------------------------------------- */
+        
+        // Set Admin 
+
+        $admin = new User();
+        $admin->setEmail('admin@admin.fr')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword('$2y$13$V9Y9X5XjzK5uNq9qIY6Q4eCJfNcZr7xXs3P8C4ZTqo8DvHjX6')
+            ->setNom('admin')
+            ->setUsername('admin')
+            ->setProfilPicture($faker->randomElement($picturePaths))
+            ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'));
+        $manager->persist($admin);
+
+
+        // Set Utilisateurs
+
+        $users = [];
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email)
+                ->setPassword('$2y$13$V9Y9X5XjzK5uNq9qIY6Q4eCJfNcZr7xXs3P8C4ZTqo8DvHjX6')
+                ->setRoles(['ROLE_USER'])
+                ->setNom($faker->lastName)
+                ->setUsername($faker->username)
+                ->setProfilPicture($faker->randomElement($picturePaths))
+                ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'));
+            $manager->persist($user);
+            array_push($users, $user);
+        }
+
+
+        /** -------------------------------------------------------------
+         *                     ENTITé PUBLICATION
+         * 
+         *  A terme permettre d'uploader les média localement. 
+         *  Mais pour le moment, on utilise des liens vers des images. 
+         *  Peut-etre rajouter le nombre d'impression de la publication 
+         *  --------------------------------------------------------------*/
+
+
+        $publications = [];
+        for ($i = 0; $i < 20; $i++) {
+            $publication = new Publication();
+            $publication->setContenu($faker->sentence(10))
+                ->setAuteur($faker->randomElement($users))
+                ->setMedia('https://placehold.co/600x400')
+                ->setLikes($faker->numberBetween(0, 100))
+                ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'));
+            $manager->persist($publication);
+            array_push($publications, $publication);
+        }
+
+        /** -------------------------------------------------------------
+         *                     ENTITé COMMENTAIRES
+         * 
+         *  Peut etre rajouter les likes de commentaires dans un second temps
+         *  
+         *  --------------------------------------------------------------*/
+
+        for ($i = 0; $i < 50; $i++) {
+            $commentaire = new Commentaire();
+            $commentaire->setContenu($faker->sentence(10))
+                ->setAuteur($faker->randomElement($users))
+                ->setPublication($faker->randomElement($publications))
+                ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'));
+            $manager->persist($commentaire);
+        }
+
+
+        /** -------------------------------------------------------------
+         *                     ENTITé MESSAGE
+         *  
+         *  Peut etre fait il rajouté un variable destinataire ?
+         *  --------------------------------------------------------------*/
+        $status = ['En cours', 'Lu', 'Archivé'];
+        for ($i = 0; $i < 20; $i++) {
+            $message = new Message();
+            $message->setContenu($faker->sentence(10))
+                ->setUtilisateur($faker->randomElement($users))
+                ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'))
+                ->setStatus($faker->randomElement($status));
+            $manager->persist($message);
+        }
+
+
+        /** -------------------------------------------------------------
+         *                     ENTITé GROUPE
+         *  
+         *  --------------------------------------------------------------*/
+
+        $groupes = [];
+        for ($i = 0; $i < 10; $i++) {
+            $groupe = new Groupe();
+            $groupe->setNom($faker->word)
+                ->setCreatedAt($faker->dateTimeBetween('now', '+2 month'))
+                ->setDescription($faker->text(100))
+                ->setCreateur($faker->randomElement($users));
+            $manager->persist($groupe);
+            array_push($groupes, $groupe);
+        }
 
         $manager->flush();
     }
