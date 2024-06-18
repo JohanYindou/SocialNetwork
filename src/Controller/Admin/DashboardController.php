23d10a2421b27c2cb9a76a646d7f60dd8cdb1424
Johan Yindou <jyindou@gmail.com>
2024-06-18 11:49:07 +0200

    "Update favicon and remove h1 tag from notifications template"

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -32,6 +32,7 @@ class DashboardController extends AbstractDashboardController
     {
         return Dashboard::new()
             ->setTitle('SocialNetwork')
+            ->setFaviconPath('/uploads/medias/Logo_SocialNetwork.png')
             ->renderContentMaximized(); // Optionally maximize the content area
     }
 
