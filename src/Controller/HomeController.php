c4c23750f81620f4ffc0af76adfade29b9e96331
Johan Yindou <jyindou@gmail.com>
2024-06-22 20:00:48 +0200

    "Added pagination to HomeController and RechercheController, and updated corresponding Twig templates to render pagination."

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PublicationRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request, PublicationRepository $publicationRepository): Response
+    public function index(
+        Request $request, 
+        PublicationRepository $publicationRepository,
+        PaginatorInterface $paginator
+        ): Response
     {
 
-        $publications = $publicationRepository->findAllOrderedByDate();
+        $pagination = $paginator->paginate(
+            $publicationRepository->findAllOrderedByDate(),
+            $request->query->getInt('page', 1),
+            5
+        );
 
         return $this->render('home/index.html.twig', [
-            'publications' => $publications,
+            'publications' => $pagination,
         ]);
     }
 }
--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Model\SearchData;
 use App\Form\RechercheType;
 use App\Repository\PublicationRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,21 +14,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class RechercheController extends AbstractController
 {
     #[Route('/recherche', name: 'app_recherche')]
-    public function search(Request $request, PublicationRepository $publicationRepository): Response
+    public function search(
+        Request $request, 
+        PublicationRepository $publicationRepository,
+        PaginatorInterface $paginator,
+    ): Response
     {
+        
         $searchData = New SearchData(); 
         $form = $this->createForm(RechercheType::class, $searchData);
         $form->handleRequest($request);
 
-        $publications = [];
-        // $searchTerm = '';
+
+        $pagination = $paginator->paginate(
+            [], // ou une valeur par défaut appropriée
+            $request->query->getInt('page', 1),
+            5
+        );
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $publications = $publicationRepository->findBySearchTerm($searchData->q);
+            $pagination = $paginator->paginate(
+                $publicationRepository->findBySearchTerm($searchData->q),
+                $request->query->getInt('page', 1),
+                5
+            );
         }
 
         return $this->render('recherche/index.html.twig', [
-            'publications' => $publications,
+            'publications' => $pagination,
             'searchData' => $searchData->q,
             'form' => $form->createView(),
         ]);
