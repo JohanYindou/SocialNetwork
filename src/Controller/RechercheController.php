ddc68f5f3cbba143fe4a3afd868606cd596ae19a
Johan Yindou <jyindou@gmail.com>
2024-06-22 18:48:45 +0200

    🐛 "Refactored RechercheController to use SearchData model and updated RechercheType form to use TextType instead of SearchType; also updated recherche/index.html.twig template to include search form and display search results"

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,33 +2,38 @@
 
 namespace App\Controller;
 
+use App\Model\SearchData;
+use App\Form\RechercheType;
 use App\Repository\PublicationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Form\RechercheType;
 
 class RechercheController extends AbstractController
 {
     #[Route('/recherche', name: 'app_recherche')]
     public function search(Request $request, PublicationRepository $publicationRepository): Response
     {
-        $searchForm = $this->createForm(RechercheType::class);
-        $searchForm->handleRequest($request);
+        $searchData = New SearchData(); 
+        $form = $this->createForm(RechercheType::class, $searchData);
+        $form->handleRequest($request);
 
         $publications = [];
-        $searchTerm = '';
+        // $searchTerm = '';
 
-        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            $searchTerm = $searchForm->get('search_term')->getData();
-            $publications = $publicationRepository->findBySearchTerm($searchTerm);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $searchTerm = $searchForm->get('searchTerm')->getData();
+            
+            // dd($searchData->q);
+            // dd($searchForm);
+            $publications = $publicationRepository->findBySearchTerm($searchData->q);
         }
 
         return $this->render('recherche/index.html.twig', [
             'publications' => $publications,
-            'searchTerm' => $searchTerm,
-            'searchForm' => $searchForm->createView(),
+            'searchData' => $searchData->q,
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/RechercheType.php
+++ src/Form/RechercheType.php
@@ -2,29 +2,32 @@
 
 namespace App\Form;
 
+use App\Model\SearchData;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RechercheType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('search_term', SearchType::class, [
-                'label' => false,
+            ->add('q', TextType::class, [
+                // 'label' => false,
                 'attr' => [
                     'placeholder' => 'Rechercher...',
                     'class' => 'form-control',
-                ],
+                ]
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+            'csrf_protection' => false
         ]);
     }
 }
